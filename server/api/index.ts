1764f954b4de726af74ec597746408777bcbb04d
Nikelesleshwaran T <nikelesh2006@gmail.com>
2026-07-14 19:41:59 +0530

    backend rectification

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,15 +1,6 @@
-import { createServer } from 'http';
 import { app, ready } from '../index';
 
 export default async function handler(req: any, res: any) {
   await ready;
-  
-  // Create a Node.js server from the Express app
-  const server = createServer(app);
-  
-  return new Promise((resolve, reject) => {
-    server.emit('request', req, res);
-    res.on('finish', resolve);
-    res.on('error', reject);
-  });
+  return app(req, res);
 }
